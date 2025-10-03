0a24f9f7220b21054fd34ec06d2ceec13f2d86d6
iwtkms <77294510+iwtkmsss@users.noreply.github.com>
2025-10-03 11:18:05 +0300

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController.cjs');
-
-router.post('/login', authController.login);
-router.post('/login/employee', authController.loginEmployee);
-
-module.exports = router;
--- server/routes/questions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const questionsController = require('../controllers/questionsController');
-
-router.get('/', questionsController.getAllQuestions);
-router.post('/', questionsController.createQuestion);
-router.delete('/:id', questionsController.deleteQuestion);
-
-module.exports = router;
